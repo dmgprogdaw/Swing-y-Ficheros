4f4b82f5a0a5a526699d7f18b33d4d2c0a5693e4
David <zukoclase96@gmail.com>
2019-08-19 14:26:52 +0200

    Al hacer clic sobre jugar selecciona una palabra al azar del fichero

--- src/Ahorcado/Juego.java
+++ src/Ahorcado/Juego.java
@@ -2,43 +2,52 @@ package Ahorcado;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
+import javax.swing.event.AncestorListener;
 
-public class Juego extends JPanel implements ActionListener {
+public class Juego extends JPanel implements ActionListener, KeyListener {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	Lienzo lienzo;
 	String letras = "abcdefghijklmnñopqrstuvwxyz";
 	private Font font;
+	private JLabel lblPalabra = null;
+	private JButton b;
 	
 	public Juego(Lienzo lienzo) throws FontFormatException, IOException {
 		this.lienzo = lienzo;
-		InputStream in = getClass().getResourceAsStream("/fuentes/heorot.ttf");
+		InputStream in = getClass().getResourceAsStream("/fuentes/Pokemon Hollow.ttf");
 		font = Font.createFont(Font.PLAIN, in).deriveFont(30f);
 		in.close();
 		
 		setLayout(new BorderLayout());
 		
 		JPanel sup = new JPanel(new GridLayout(1, 1));
-		JLabel lblPalabra = new JLabel("PALABRA");
+		lblPalabra = new JLabel("PALABRA");
 		lblPalabra.setBorder(BorderFactory.createCompoundBorder(
 				BorderFactory.createEmptyBorder(30, 30, 30, 30),
 				BorderFactory.createCompoundBorder(
@@ -53,13 +62,18 @@ public class Juego extends JPanel implements ActionListener {
 				BorderFactory.createEmptyBorder(30, 30, 30, 30),
 				BorderFactory.createBevelBorder(BevelBorder.RAISED)));
 		for (int i=0; i<letras.length(); i++) {
-			JButton b = new JButton("" + letras.charAt(i)); //o usar (letras.subString(i, i + 1))
+			b = new JButton("" + letras.charAt(i)); //o usar (letras.subString(i, i + 1))
 			b.setFont(font);
 			inf.add(b);
 			b.setEnabled(false);//Todas la letras estan deshabilitadas
+			b.addActionListener(this);
+			b.setActionCommand("JUGAR");
 		}
-		inf.add(new JButton("Jugar"));
-
+//		inf.add(new JButton("Jugar"));
+		JButton jugar = new JButton("Jugar");
+		jugar.setActionCommand("JUGAR");
+		jugar.addActionListener(this);
+		inf.add(jugar);
 
 		add(sup, BorderLayout.CENTER);
 		add(inf, BorderLayout.SOUTH);
@@ -67,6 +81,51 @@ public class Juego extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("JUGAR")) {
+			b.setEnabled(true);
+			ArrayList<String> palabras = new ArrayList<String>();
+			FileReader fl = null;
+			try {
+				fl = new FileReader("C:/users/usuario/git/Swing-y-Ficheros/res/palabras.txt");
+			} catch (FileNotFoundException e2) {
+				System.out.println("El fichero no ha sido encontrado");
+			}
+			BufferedReader br = new BufferedReader(fl);
+			String linea = null;
+			try {
+				while ((linea = br.readLine()) != null) {
+					String palabra = linea;
+					palabras.add(palabra);
+				}
+				Random r = new Random();
+				Object array [] = palabras.toArray();
+				String palabraAzar = (String) array[r.nextInt(palabras.size())];
+				int cantidadLetras = palabraAzar.length();
+				String simbolo = " _ ";
+				for(int i=0; i<cantidadLetras; i++) {
+					System.out.print(simbolo);
+				}
+					
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
